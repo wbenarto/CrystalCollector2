c739788f9b964bb6c69b050a53fa34e00ba0a570
w.benarto <w.benarto@gmail.com>
2019-05-26 18:16:38 -0700

    6/23/19

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// Start game score = 0
+// each button will get random values from 1 - 12
+$(document).ready(function() {
+
+})
+
+let score = 0;
+let wins = 0;
+let losses = 0;
+
+function startGame() {
+let thanos = Math.random(Math.floor(),120)
+
+}
+
+let randomNum()
